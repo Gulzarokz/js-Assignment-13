bf1ca40c3fffcb214d6e62678ac25e589f8008c4
Gulzarokz <gokz22301@gamil.com>
2024-10-31 21:24:12 +0500

    done string to number and numbert to string also control length of decimal in flooting

---
+++ index.js
@@ -0,0 +1,61 @@
+//29 Converting strings to numbers, numbers to strings
+
+
+// converting string to number 
+// const numString = "34";
+// // console.log(typeof numString);
+// const convertedNumber = Number(numString);
+// console.log(typeof convertedNumber);
+
+
+// converted number to string
+
+// const newNumber = 33;
+// // console.log(typeof newNumber);
+// const convertedString = newNumber.toString();
+// console.log(typeof convertedString);
+
+
+// 30:Controlling the length of decimals
+
+// const itemPrice = 85.456;
+// console.log(itemPrice.toFixed(2));
+
+// const itemPrice = 1750.84;
+// const sellTex = 0.02;
+// const sellTexAmount = itemPrice * sellTex;
+// // console.log(sellTexAmount);
+
+// const totalPrice = itemPrice + sellTexAmount;
+// console.log(totalPrice.toFixed(0));
+
+// School world real exapmple;
+
+// const totalMarks = 1200;
+// const obrainedMarks = 860;
+
+// // before limiting the length
+// // let percentage = ((obrainedMarks / totalMarks) * 100);
+// // console.log(percentage);
+
+// let percentage = ((obrainedMarks / totalMarks) * 100).toFixed(2);
+// console.log(percentage);
+
+// console.log(1.5555.toFixed(2));
+
+const num = 1.555;
+let str = num.toString();
+// console.log(typeof str);
+
+if (str.charAt(str.length - 1) === "5");
+
+str = str.slice(0, length - 1) + "6";
+
+console.log(num);
+
+
+console.log(str);
+
+
+
+
